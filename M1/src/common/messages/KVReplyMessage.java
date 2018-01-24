8b4950b880540b2c70a20a85965d23a272a3606f
Serah  Angel <serahang@ug132.eecg.toronto.edu>
2018-01-24 17:31:45 -0500

    Returned status as a string

--- M1/src/common/messages/KVReplyMessage.java
+++ M1/src/common/messages/KVReplyMessage.java
@@ -39,4 +39,40 @@ public class KVReplyMessage implements KVMessage
     public StatusType getStatus() {
         return status;
     }
+	
+    @Override
+    public String getStatusString() {
+	switch(this.getStatus()) {
+	    case this.StatusType.GET:
+		return "GET";
+	      
+	    case this.StatusType.GET_SUCCESS:
+		return "GET_SUCCESS";
+	    
+	    case this.StatusType.GET_ERROR:
+		return "GET_ERROR";
+	  
+	    case this.StatusType.PUT:
+		return "PUT";
+	
+	    case this.StatusType.PUT_SUCCESS:
+		return "PUT_SUCCESS";
+
+	    case this.StatusType.PUT_ERROR:
+		return "PUT_ERROR";
+
+	    case this.StatusType.PUT_UPDATE:
+		return "PUT_UPDATE";
+
+	    case this.StatusType.DELETE_SUCCESS:
+		return "DELETE_SUCCESS";
+
+	    case this.StatusType.DELETE_ERROR:
+		return "DELETE_ERROR";
+
+	    default:
+		return NULL;
+	}
+	
+    }	
 }
