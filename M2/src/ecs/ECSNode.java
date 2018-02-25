9eaa7ceefc53812d8bf3df9f23a16334438181e6
Serah  Angel <serahang@ug206.eecg.toronto.edu>
2018-02-25 12:42:20 -0500

    ECS Node implementation

---
+++ M2/src/ecs/ECSNode.java
@@ -0,0 +1,33 @@
+
+package ecs;
+
+public class ECSNode {
+
+    /**
+     * @return  the name of the node (ie "Server 8.8.8.8")
+     */
+    public String getNodeName() {
+		return this.meta.nodeName;
+	}
+
+    /**
+     * @return  the hostname of the node (ie "8.8.8.8")
+     */
+    public String getNodeHost() {
+		return this.meta.addr;
+	}
+    /**
+     * @return  the port number of the node (ie 8080)
+     */
+    public int getNodePort() {
+		return this.meta.port;
+	}
+    /**
+     * @return  array of two strings representing the low and high range of the hashes that the given node is responsible for
+     */
+    public String[] getNodeHashRange() {
+		String [] hashRange = {this.meta.bHash, this.meta.eHash};
+		return hashRange;	
+	}
+
+}
