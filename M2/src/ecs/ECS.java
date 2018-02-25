22ca0e3944e156dae5c11c1f708de0797973f33a
Elsayed  Aya Ehab Elsayed Mahmoud <elsaye10@ug207.eecg.toronto.edu>
2018-02-25 16:16:02 -0500

    parsing config file

--- M2/src/ecs/ECS.java
+++ M2/src/ecs/ECS.java
@@ -2,24 +2,53 @@ package ecs;
 import app_kvServer.KVServer;
 import IECSNode;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.MessageDigest;
 
 public class ECS {
 
 	
 	private confPath; 
 	private Socket socket;
-	
+    private String DELIM = "|";
+    private HashMap ringNetwork;
+	private enum ConfigContent {
+        SERVER_NAME = 0,
+        SERVER_IP = 1,
+        SERVER_PORT = 2;
+    }
 	public ECS(String confFile)
     {
-        this.configFile = Paths.get(confPath
-
-        try {
-        	
-		}
-
+        this.configFile = Paths.get(configFile);
+        this.ringNetwork = new HashMap();
+        populateRingNetwork();
     }
 
+    private BigInteger strToMD5(String m){
+        MessageDigest hash = MessageDigest.getInstance("MD5");
+
+        //hash.update(input, offset, len)
+        hash.update(m.getBytes(), 0, m.length());
+
+        return new BigInteger(1, hash.digest());
+    }
+
+    private void populateRingNetwork() throws IOException {
+        ArrayList<String> lines = new ArrayList<>(Files.readAllLines(this.configFile, standardCharsets.UTF_8));
+        int numServers = Math.floor(lines.size()/2);
+        
+        for(int i = 0; i < numServers ; i++) {
+            String[] line = lines.get(i).split(" ");
+            BigInteger serverHash = strToMD5(line[SERVER_NAME] + DELIM + line[SERVER_IP] + DELIM + line[SERVER_PORT]);
+            //TODO figure out begin and end hash, does zookeeper assign it?
+            ECSNode ecsNode = new ECSNode(line[SERVER_NAME], line[SERVER_IP], Integer.parseInt(line[SERVER_PORT]), null, null)
+            this.ringNetwork.put(serverHash, ecsNode);
+        }
+
+    }
 
 	public void addNode() {
 
--- M2/src/ecs/ECSNode.java
+++ M2/src/ecs/ECSNode.java
@@ -1,12 +1,17 @@
 package ecs;
 
-public class ECSNode {
+import common.ServerMetaData;
 
+public class ECSNode {
+    private ServerMetaData meta;
+    public ECSNode(String name, String addr, int port, String bHash, String eHash) {
+        this.meta = new ServerMetaData(name, addr, port, bHash, eHash);
+    }
     /**
      * @return  the name of the node (ie "Server 8.8.8.8")
      */
     public String getNodeName() {
-		return this.meta.nodeName;
+		return this.meta.name;
 	}
 
     /**
