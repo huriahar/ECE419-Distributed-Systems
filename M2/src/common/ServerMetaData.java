f3a3ea13ca441ec8776cffb6bb7da1ff5b2eb6c3
Elsayed  Aya Ehab Elsayed Mahmoud <elsaye10@ug207.eecg.toronto.edu>
2018-02-25 12:40:55 -0500

    added name to metadata

--- M2/src/common/ServerMetaData.java
+++ M2/src/common/ServerMetaData.java
@@ -1,20 +1,22 @@
 package common;
 
 public class ServerMetaData {
+    public String name;
     public String addr;
     public int port;
     public String bHash;
     public String eHash;
 
-    public ServerMetaData(String addr, int port, String bHash, String eHash) {
+    public ServerMetaData(String name, String addr, int port, String bHash, String eHash) {
+        this.name = name;
         this.addr = addr;
         this.port = port;
         this.bHash = bHash;
         this.eHash = eHash;
     }
 
-    public ServerMetaData(String addr, int port) {
-        this(addr, port, null, null);
+    public ServerMetaData(String, name, String addr, int port) {
+        this(name, addr, port, null, null);
     }
 }
 
