e773ce69f8fb2c61fbaf53e382ea2ad6abb52580
Serah  Angel <serahang@ug206.eecg.toronto.edu>
2018-03-01 21:04:37 -0500

    Added some functionality in ECS

--- M2/src/ecs/ECS.java
+++ M2/src/ecs/ECS.java
@@ -3,50 +3,76 @@ package ecs;
 import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.Map;
+import java.net.Socket;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import java.io.OutputStream;
+import java.io.InputStream;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 import java.math.BigInteger;
 import java.util.Collection;
 
 import org.apache.log4j.Logger;
 import java.lang.Process;
-//import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooKeeper;
 
 import common.*;
+import common.messages.TextMessage;
 
+import ecs.CreateGroup;
 
 public class ECS {
-    private Path configFile; 
+    private Path configFile;
+    private Path metaDataFile;
+    private Socket ECSSocket;
+
+    private static final int BUFFER_SIZE = 1024;
+    private static final int DROP_SIZE = 128 * BUFFER_SIZE;
+    public static final String metaFile = "metaDataECS.config";            
+    
     private TreeMap<String, IECSNode> ringNetwork;
     private static Logger logger = Logger.getRootLogger();
-//    private ZooKeeper zk;
+    private OutputStream output; 
+    private InputStream input;
+        
+    private ZooKeeper zk;
     
 	public ECS(String configFile) {
         this.configFile = Paths.get(configFile);
         this.ringNetwork = new TreeMap<String, IECSNode>();
         try {
-			populateRingNetwork();
+            if(!Files.exists(Paths.get(metaFile)))
+                this.metaDataFile = Files.createFile(Paths.get(metaFile));
+            else {
+			    this.metaDataFile = Paths.get(metaFile);    
+                populateRingNetwork();
+            }
+            
 		} catch (IOException e) {
-			logger.error("Unable to populate ring network. Exception " + e);
+			logger.error("Unable to open metaDataFile " + e);
 		}
     }
 
     private void populateRingNetwork()
     		throws IOException {
-        ArrayList<String> lines = new ArrayList<>(Files.readAllLines(this.configFile, StandardCharsets.UTF_8));
-        int numServers = (int) Math.floor(lines.size()/2);		// Why??????
+        ArrayList<String> lines = new ArrayList<>(Files.readAllLines(this.metaDataFile, StandardCharsets.UTF_8));
+        int numServers = lines.size();		
         
         for (int i = 0; i < numServers ; ++i) {
             String[] line = lines.get(i).split(" ");
-            String serverHash = md5.encode(line[0] + KVConstants.DELIM + line[1] + KVConstants.DELIM + line[2]);
-            //TODO figure out begin and end hash, does zookeeper assign it? No it doesn't. Have to do it ourselves
-            IECSNode node = new ECSNode(line[0], line[1], Integer.parseInt(line[2]), null, null);
+            String serverHash = md5.encode(line[ServerMetaData.SERVER_NAME] + KVConstants.DELIM +
+                                           line[ServerMetaData.SERVER_IP] + KVConstants.DELIM +
+                                           line[ServerMetaData.SERVER_PORT]);
+            IECSNode node = new ECSNode(line[ServerMetaData.SERVER_NAME], line[ServerMetaData.SERVER_IP], Integer.parseInt(line[ServerMetaData.SERVER_PORT]), line[ServerMetaData.BEGIN_HASH], line[ServerMetaData.END_HASH]);
             this.ringNetwork.put(serverHash, node);
         }
     }
@@ -58,15 +84,72 @@ public class ECS {
     public boolean start(IECSNode server) {
         // TODO
         boolean failed = false;
-        //Loop through the list of nodes in Collections and change their status away from STOPPED   
+        TextMessage message, response; 
+            
+//        for(Map.Entry<String, IECSNode> iter: ringNetwork.entrySet()) {
+            try { 
+//                server = iter.getValue();
+                message = new TextMessage("ECS" + KVConstants.DELIM + "START_NODE");
+                connect(server);
+                sendMessage(message);
+                response  = receiveMessage();
+                disconnect(server);
+                if(response.getMsg().equals("START_SUCCESS")) 
+                    logger.info("Started KVServer: " + server.getNodeName() + " <" + server.getNodeHost() + "> <" + 
+                    server.getNodePort() + ">"); 
+            } catch (IOException io) {
+			    logger.error("START ERROR for KVServer: " + server.getNodeName() + ". Error: " + io);
+                failed = true;
+            } 
+           
+  //      }
+            
         return failed;
     }
 
+
+    public void connect(IECSNode server) throws IOException {
+        this.ECSSocket = new Socket(server.getNodeHost(), server.getNodePort());
+        logger.info("ECS Connection to name: " + server.getNodeName() + " successful");
+    }
+
+    public void disconnect(IECSNode server) {
+        try { 
+
+            if(ECSSocket != null) {
+                ECSSocket.close();
+                ECSSocket = null;
+                logger.info("Successfully closed ECS connection to server: " + server.getNodeName());
+
+            }
+        } catch (IOException io) {
+            logger.error("Unable to close ECS to server connection");
+        }
+
+    }
+        
+        
     public boolean stop(IECSNode server) {
         // TODO
-        //Loop through the list of ECS
-        return false;
-
+        boolean failed = false;
+        TextMessage message, response; 
+        
+        try { 
+            message = new TextMessage("ECS" + KVConstants.DELIM + "STOP_NODE");
+            connect(server);
+            sendMessage(message);
+            response = receiveMessage();
+            disconnect(server);
+            if(response.getMsg().equals("STOP_SUCCESS")) 
+                logger.info("Stop KVServer: " + server.getNodeName() + " <" + server.getNodeHost() + "> <" + 
+                server.getNodePort() + ">"); 
+        } catch (IOException ex) {
+		    logger.error("STOP ERROR for KVServer: " + server.getNodeName() + ". Error: " +  ex);
+            failed = true;
+        } 
+           
+            
+        return failed;
     }
 
 
@@ -78,35 +161,32 @@ public class ECS {
 
     public IECSNode addNode(String cacheStrategy, int cacheSize) {
         // TODO
+        //Add a node in own metadata, reassign hashing and send a TextMessage to all Servers 
+        // to update their metaData (UPDATE_METADATA)
         return null;
     }
 
-    /**
-     * Randomly choose <numberOfNodes> servers from the available machines and start the KVServer by issuing an SSH call to the respective machine.
-     * This call launches the storage server with the specified cache size and replacement strategy. For simplicity, locate the KVServer.jar in the
-     * same directory as the ECS. All storage servers are initialized with the metadata and any persisted data, and remain in state stopped.
-     * NOTE: Must call setupNodes before the SSH calls to start the servers and must call awaitNodes before returning
-     * @return  set of strings containing the names of the nodes
-     */
     public boolean initService(Collection<IECSNode> nodes, String cacheStrategy, int cacheSize) {
         // TODO
     
-        Process proc;
-        String script = "script.sh";
+       // Process proc;
+       // String script = "script.sh";
         boolean success = true;
 
-        for(IECSNode node: nodes) {
-            //SSHCall for each server and launch with the right size and strategy
-            Runtime run = Runtime.getRuntime();
-            String nodeHost = node.getNodeHost();
-            String nodePort = Integer.toString(node.getNodePort());
-            String cmd = "script " + nodeHost + " " + nodePort;
-            try {
-              proc = run.exec(cmd);
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-        }
+       // for(IECSNode node: nodes) {
+       //     //SSHCall for each server and launch with the right size and strategy
+       //     Runtime run = Runtime.getRuntime();
+       //     String nodeHost = node.getNodeHost();
+       //     String nodePort = Integer.toString(node.getNodePort());
+       //     String cmd = "script " + nodeHost + " " + nodePort;
+       //     try {
+       //       proc = run.exec(cmd);
+       //     } catch (IOException e) {
+       //       e.printStackTrace();
+       //     }
+       // }
+
+        //Initialize own hashRing
 
         return success; 
     }
@@ -141,6 +221,87 @@ public class ECS {
         }
         serverNode = ringNetwork.higherEntry(encodedKey).getValue();
         return serverNode;
+    }
+
+
+    /**
+     * Method sends a TextMessage using this socket.
+     * @param msg the message that is to be sent.
+     * @throws IOException some I/O error regarding the output stream 
+     */
+    public void sendMessage(TextMessage msg)
+            throws IOException {
+        byte[] msgBytes = msg.getMsgBytes();
+        output.write(msgBytes, 0, msgBytes.length);
+        output.flush();
+        logger.info("SEND \t<" 
+                + ECSSocket.getInetAddress().getHostAddress() + ":" 
+                + ECSSocket.getPort() + ">: '" 
+                + msg.getMsg() +"'");
+    }
+
+    private TextMessage receiveMessage()
+            throws IOException {
+        int index = 0;
+        byte[] msgBytes = null, tmp = null;
+        byte[] bufferBytes = new byte[BUFFER_SIZE];
+        
+        /* read first char from stream */
+        byte read = (byte) input.read();    
+        boolean reading = true;
+
+        while(read != 10 && read != -1 && reading) {/* CR, LF, error */
+            /* if buffer filled, copy to msg array */
+            if(index == BUFFER_SIZE) {
+                if(msgBytes == null){
+                    tmp = new byte[BUFFER_SIZE];
+                    System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+                } else {
+                    tmp = new byte[msgBytes.length + BUFFER_SIZE];
+                    System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+                    System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+                            BUFFER_SIZE);
+                }
+
+                msgBytes = tmp;
+                bufferBytes = new byte[BUFFER_SIZE];
+                index = 0;
+            } 
+            
+            /* only read valid characters, i.e. letters and constants */
+            bufferBytes[index] = read;
+            index++;
+            
+            /* stop reading is DROP_SIZE is reached */
+            if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+                reading = false;
+            }
+            
+            /* read next char from stream */
+            read = (byte) input.read();
+        }
+        
+        if(msgBytes == null){
+            tmp = new byte[index];
+            System.arraycopy(bufferBytes, 0, tmp, 0, index);
+        }
+        else {
+            tmp = new byte[msgBytes.length + index];
+            System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+            System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+        }
+        
+        msgBytes = tmp;
+
+        if (new String(msgBytes).trim().isEmpty()) throw new IOException();
+        
+        /* build final String */
+        TextMessage msg = new TextMessage(msgBytes);
+        logger.info("RECEIVE \t<" 
+                + ECSSocket.getInetAddress().getHostAddress() + ":" 
+                + ECSSocket.getPort() + ">: '" 
+                + msg.getMsg().trim() + "'");
+        return msg;
     }
 
 
