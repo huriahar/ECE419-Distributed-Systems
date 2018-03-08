2988def3794cebec9052df74aa9013061b4b5716
Harshita Huria <harshita.huria@mail.utoronto.ca>
2018-03-08 12:16:17 -0500

    Can read log messages from server nowgit add script.sh!

--- M2/script.sh
+++ M2/script.sh
@@ -1,2 +1,2 @@
-ssh -n localhost nohup java -jar ~/ece419-ECE419-Distributed-Systems/M2/m2-server.jar $1 $2 $3 ERROR
+nohup java -jar ~/ece419/ECE419-Distributed-Systems/M2/m2-server.jar $1 $2 $3 > "out$1.txt" 2>&1 &
 
